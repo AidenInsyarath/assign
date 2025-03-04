5e21a0ef3358b5d0dac24fd9d1343ca816430948
AidenInsyarath <Goldenpig02@gmail.com>
2025-03-04 10:38:41 -0800

    some text

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,4 @@
-const loadText = document.quuerySelector('.loading-text')
+const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
@@ -13,7 +13,7 @@ function blurring(){
     if (load >= 100){
         clearInterval(int)
     }
-    loadText.innerText = `${load}% `
+    loadText.innerText = `${load}%`
     bg.style.filter = `blur(${50 - (load / 2)}px)`;
-    loadingText.style.opacity = `${1 - load/100 }`;
+    loadText.style.opacity = `${1 - (load / 100)}`;
 }
