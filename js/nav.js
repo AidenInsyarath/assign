7d6b904cf56517423caa332e208d661445666473
AidenInsyarath <Goldenpig02@gmail.com>
2025-03-17 16:16:42 +0000

    Mon, Mar 17, 2025, 9:16 AM -07:00

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector('.nav')
 
 window.addEventListener('scroll', () => {
-    if (windo.scrollY > 160){
+    if (window.scrollY > 160){
         nav.classList.add('active)
     }
     else {
