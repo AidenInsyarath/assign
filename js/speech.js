0e0ee740c41452d61554ad5b20e69283e87b079c
AidenInsyarath <Goldenpig02@gmail.com>
2025-05-16 17:13:36 +0000

    Fri, May 16, 2025, 10:13 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -1,1 +1,58 @@
+const main = document.querySelector('main')
+const voicesSelect = document.getElementById('voices')
+const textarea = document.getElementById('text')
+const readBtn = document.getElementById('read')
+const toggleBtn = document.getElementById('toggle')
+const closeBtn = document.getElementById('close')
 
+data[0].image
+const data = [
+    {
+        image:'../drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: '../food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: '../tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: '../hurt.jpg",
+        text: "I'm Hurt"
+    },
+    {
+        image: '../happy.jpg",
+        text: "I'm Happy"
+    },
+    {
+        image: '../angry.jpg",
+        text: "I'm Angry"
+    },
+    {
+        image: '../sad.jpg",
+        text: "I'm Sad"
+    },
+    {
+        image: '../scared.jpg",
+        text: "I'm Scared"
+    },
+    {
+        image '../outside.jpg",
+        text:"I Want To Go Outside"
+    },
+    {
+        image:'../home.jpg',
+        text "I Want To Go Home"
+    },
+    {
+        image: '../school.jpg',
+        text "I want To Go To School"
+    },
+    {
+        image: '../grandma.jpg',
+        text: "
+    }
+]
