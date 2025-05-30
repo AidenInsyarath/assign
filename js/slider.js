26c50d46d73ac35acd7f8a5830a55a4338341a46
AidenInsyarath <Goldenpig02@gmail.com>
2025-05-30 07:26:03 +0000

    Fri, May 30, 2025, 12:26 AM -07:00

---
+++ js/slider.js
@@ -0,0 +1,6 @@
+ const upButton = document.querySelector ('.up-button')
+ const downButton = document. querySelector('down-button')
+ const rightSide = document. querySelector('right-side')
+ const leftSide = document. querySelector('left-side')
+ const slidesLength = rightSide.querySelectorAll('div').length
+ upButton.addEventListener('click', () => changeSlide('up'))
