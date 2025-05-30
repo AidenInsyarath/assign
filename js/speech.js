8d7539a3e88d563d7942aef0972b6a9bda0d0f80
AidenInsyarath <Goldenpig02@gmail.com>
2025-05-30 10:03:51 -0700

    some text

--- js/speech.js
+++ js/speech.js
@@ -7,51 +7,51 @@ const closeBtn = document.getElementById('close')
 
 const data = [
     {
-        image:'../drink.jpg',
+        image:'drink.jpg',
         text: "I'm Thirsty"
     },
     {
-        image: '../food.jpg',
+        image: 'food.jpg',
         text: "I'm Hungry"
     },
     {
-        image: '../tired.jpg',
+        image: 'tired.jpg',
         text: "I'm Tired"
     },
     {
-        image: '../hurt.jpg',
+        image: 'hurt.jpg',
         text: "I'm Hurt"
     },
     {
-        image: '../happy.jpg',
+        image: 'happy.jpg',
         text: "I'm Happy"
     },
     {
-        image: '../angry.jpg',
+        image: 'angry.jpg',
         text: "I'm Angry"
     },
     {
-        image: '../sad.jpg',
+        image: 'sad.jpg',
         text: "I'm Sad"
     },
     {
-        image: '../scared.jpg',
+        image: 'scared.jpg',
         text: "I'm Scared"
     },
     {
-        image: '../outside.jpg',
+        image: 'outside.jpg',
         text:"I Want To Go Outside"
     },
     {
-        image: '../home.jpg',
+        image: 'home.jpg',
         text: "I Want To Go Home"
     },
     {
-        image: '../school.jpg',
+        image: 'school.jpg',
         text: "I want To Go To School"
     },
     {
-        image: '../grandma.jpg',
+        image: 'grandma.jpg',
         text: "I Want To Go To Grandmas"
     }
 ]
