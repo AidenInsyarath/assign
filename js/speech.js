796b9542ea1faf6b7f5fd60df241c5c135e95c1e
AidenInsyarath <Goldenpig02@gmail.com>
2025-05-30 07:11:51 +0000

    Fri, May 30, 2025, 12:11 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -65,5 +65,75 @@ function createBox(item) {
         <img src="${item.image}" alt="${item.text}" />
         <p class="info"> ${item.text} </p>
     `
+
+
+    box.addEventListener('click', () => {
+        setTextMessage(item.text)
+        speakText()
+
+        // Add active effect
+        box.classList.add('active')
+        setTimeout(() => box.classList.remove('active'), 800)
+    })
+
     main.appendChild(box)
 }
+
+//Initialize Speech Synthesis
+const message = new SpeechSynthesisUtterance()
+
+//Set Text
+function setTextMessage(text) {
+    message.text = text
+}
+
+//Speak Text
+function speakText() {
+    speechSynthesis.speak(message)
+}
+
+
+// Toggle Text box
+toggleBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.toggle('show')
+})
+
+//Close Box
+closeBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.remove('show')
+})
+
+let voices = []
+
+function getVoices() {
+    voices = speechSynthesis.getVoices()
+
+    voices.forEach(voice => {
+        const option = document.createElement('option')
+
+        option.value = voice.name
+        option.innerText = `${voice.name} ${voice.lang}`
+
+        voicesSelect.appendChild(option)
+    })
+}
+
+//voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+
+// Change Voice
+voicesSelect.addEventListener('change', setVoice)
+
+// Set Voice
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value)
+}
+
+getVoices()
+
+// Read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value)
+    speakText()
+})
